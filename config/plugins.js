b43eae713c07b036c30eaca8c11f0b41640a45d1
Matviy <antipin.m9971@gmail.com>
2023-12-10 19:35:34 +0200

    add plugins

---
+++ config/plugins.js
@@ -0,0 +1,30 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+
+  email: {
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: "antipin.m9971@gmail.com",
+        defaultReplyTo: "antipin.m9971@gmail.com",
+      },
+    },
+  },
+});
